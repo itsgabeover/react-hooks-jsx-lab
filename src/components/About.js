2e47e2473b17beeb057b713de5c9fdb14c573d2c
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-08-31 11:05:32 -0700

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,19 @@
 import React from "react";
+// eslint-disable-next-line
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h1>
+        About Me
+      </h1>
+      <p>
+        I'm learning to code!
+      </p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  )
 }
 
 export default About;
